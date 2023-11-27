2f0ed06879d4249207eea29c9d8248cd03c45703
Aafaq Ahmad <malikafaq788@gmail.com>
2023-11-27 22:30:33 +0500

    selected features

--- src/app/page.js
+++ src/app/page.js
@@ -7,227 +7,35 @@ import { AiOutlineEye } from 'react-icons/ai'
 import { FiPlus } from 'react-icons/fi'
 import { IoMdAdd } from 'react-icons/io'
 import { MdDeleteOutline } from 'react-icons/md'
+import { FaArrowLeftLong } from 'react-icons/fa6'
+import { FaCircleCheck } from 'react-icons/fa6'
+import { sidebarData } from './data'
 
 export default function App () {
   const [selectedFeature, setSelectedFeature] = useState(null)
-  const sidebarData = [
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png',
-      dropDown: [
-        {
-          name: 'Activity Log',
-          mobile:
-            'https://builderbuckets.blob.core.windows.net/builder-now-production/uploads/production/feature_figma/image/1031/3100e295-6ea2-4979-a36d-fb0ae986d628.png',
-          cost: '$150',
-          category: 'Healthcare compliance',
-          details:
-            'Capture actions performed by users inside the product, storing the raw data to use in product analytics. This helps to get a better insight into how users use the product.',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/59fd145e88f3ac61106b2fa0/Activity_20Log.png'
-        },
-        {
-          name: 'Admin Console',
-          mobile:
-            'https://builderbuckets.blob.core.windows.net/builder-now-production/uploads/production/feature_figma/image/1745/2d25fafc-11ba-4843-8667-95254d618cdb.png',
-          cost: '$160',
-          category: 'Healthcare compliance',
-          details:
-            'The primary control panel for the product and crucial for admins. Achieve tasks such as managing users, updating content, controlling roles and permissions and more.',
-          time: '3.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/icon/59fd164788f3ac61106b3545/Admin_Console.svg'
-        },
-        {
-          name: 'Analytics',
-          mobile:
-            'https://builderbuckets.blob.core.windows.net/builder-now-production/uploads/production/feature_figma/image/53/1a760b16-496c-4e71-b389-83eeffd74e60.png',
-          cost: '$120',
-          category: 'Healthcare compliance',
-          details:
-            'Gain insights into how your customers interact with the product. These insights allow business decisions to be led by data, providing the information needed to improve crucial product factors. Needs a third-party integration, so the final cost depends on their pricing.',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/57e0aff95cba2a27d6af2784/Analytics.png'
-        },
-        {
-          name: 'Analytics',
-          mobile:
-            'https://builderbuckets.blob.core.windows.net/builder-now-production/uploads/production/feature_figma/image/53/1a760b16-496c-4e71-b389-83eeffd74e60.png',
-          cost: '$120',
-          category: 'Healthcare compliance',
-          details:
-            'Gain insights into how your customers interact with the product. These insights allow business decisions to be led by data, providing the information needed to improve crucial product factors. Needs a third-party integration, so the final cost depends on their pricing.',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/57e0aff95cba2a27d6af2784/Analytics.png'
-        },
-        {
-          name: 'Analytics',
-          mobile:
-            'https://builderbuckets.blob.core.windows.net/builder-now-production/uploads/production/feature_figma/image/53/1a760b16-496c-4e71-b389-83eeffd74e60.png',
-          cost: '$120',
-          category: 'Healthcare compliance',
-          details:
-            'Gain insights into how your customers interact with the product. These insights allow business decisions to be led by data, providing the information needed to improve crucial product factors. Needs a third-party integration, so the final cost depends on their pricing.',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/57e0aff95cba2a27d6af2784/Analytics.png'
-        },
-        {
-          name: 'Analytics',
-          mobile:
-            'https://builderbuckets.blob.core.windows.net/builder-now-production/uploads/production/feature_figma/image/53/1a760b16-496c-4e71-b389-83eeffd74e60.png',
-          cost: '$120',
-          category: 'Healthcare compliance',
-          details:
-            'Gain insights into how your customers interact with the product. These insights allow business decisions to be led by data, providing the information needed to improve crucial product factors. Needs a third-party integration, so the final cost depends on their pricing.',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/57e0aff95cba2a27d6af2784/Analytics.png'
-        }
-      ]
-    },
-    {
-      name: 'Essentials',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/59fd274188f3ac56ef461c73/Essentials.svg',
-      dropDown: [
-        {
-          name: 'Activity Log',
-          cost: '$150',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/59fd145e88f3ac61106b2fa0/Activity_20Log.png'
-        },
-        {
-          name: 'Admin Console',
-          cost: '$160',
-          time: '3.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/icon/59fd164788f3ac61106b3545/Admin_Console.svg'
-        },
-        {
-          name: 'Analytics',
-          cost: '$120',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/57e0aff95cba2a27d6af2784/Analytics.png'
-        }
-      ]
-    },
-    {
-      name: 'Content',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/59fd271588f3ac56ef461c64/Content.svg'
-    },
-    {
-      name: 'Social',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/59fd272f88f3ac56ef461c6d/Social.svg',
-      dropDown: [
-        {
-          name: 'Activity Log',
-          cost: '$150',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/59fd145e88f3ac61106b2fa0/Activity_20Log.png'
-        },
-        {
-          name: 'Admin Console',
-          cost: '$160',
-          time: '3.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/icon/59fd164788f3ac61106b3545/Admin_Console.svg'
-        },
-        {
-          name: 'Analytics',
-          cost: '$120',
-          time: '4.0 days',
-          icon: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature/new_icon/57e0aff95cba2a27d6af2784/Analytics.png'
-        }
-      ]
-    },
-    {
-      name: 'Admin',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/59fd273e88f3ac56ef461c72/Admin_Console.svg'
-    },
-    {
-      name: 'Ecommerce',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/59fd272488f3ac56ef461c69/Ecommerce.svg'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
-    },
-    {
-      name: 'Healthcare compliance',
-      img: 'https://bstudio-assets.azureedge.net/assets-builder/uploads/feature_bundle/background/24/medical-folder.png'
+  const [selectedFeatures, setSelectedFeatures] = useState([])
+
+  const handleFeaturesSelection = feature => {
+    console.log('feature', feature.name)
+
+    // Check if the feature is already selected
+    const isSelected = selectedFeatures.some(
+      selected => selected.name === feature.name
+    )
+
+    if (isSelected) {
+      // If selected, remove it from the array
+      const updatedFeatures = selectedFeatures.filter(
+        selected => selected.name !== feature.name
+      )
+      setSelectedFeatures(updatedFeatures)
+    } else {
+      // If not selected, add it to the array
+      setSelectedFeatures(prevFeatures => [...prevFeatures, feature])
     }
-  ]
+
+    setSelectedFeature(feature)
+  }
 
   const [activeDropdown, setActiveDropdown] = useState(null)
 
@@ -244,6 +52,10 @@ export default function App () {
     setSelectedFeature(feature)
   }
 
+  const isFeatureSelected = feature => {
+    return selectedFeatures.some(selected => selected.name === feature.name)
+  }
+
   return (
     <div className='flex min-h-screen'>
       {/* Sidebar */}
@@ -253,7 +65,7 @@ export default function App () {
             <div
               key={index}
               onClick={() => toggleDropdown(index)}
-              className='group hover:bg-slate-200 border-b-2 p-4 cursor-pointer transition duration-500 ease-in-out'
+              className='group hover:bg-slate-100 border-b-2 p-4 cursor-pointer transition duration-500 ease-in-out'
             >
               <div className='flex justify-between items-center text-black'>
                 <div className='flex items-center gap-3'>
@@ -265,7 +77,7 @@ export default function App () {
                   <p className='text-sm'>{item.name}</p>
                 </div>
                 {item.dropDown && activeDropdown != index && (
-                  <MdKeyboardArrowDown className='group-hover:text-black text-xl text-white' />
+                  <MdKeyboardArrowDown className='group-hover:text-black text-xl text-transparent' />
                 )}
                 {activeDropdown === index && (
                   <MdKeyboardArrowUp className='text-black text-xl' />
@@ -289,15 +101,26 @@ export default function App () {
                 item.dropDown.map((item, index) => (
                   <div
                     key={index}
-                    className='flex flex-grow w-full cursor-pointer hover:border-l-gray-500 border-l-4 border-l-white hover:border-l-4 justify-between items-center p-4 border-b-[1px] hover:bg-slate-200 text-black'
+                    onClick={() => handleFeatureSelection(item)}
+                    className={`flex flex-grow w-full cursor-pointer ${
+                      isFeatureSelected(item)
+                        ? 'border-l-blue-500'
+                        : selectedFeature?.name === item.name
+                        ? 'border-l-gray-500'
+                        : 'hover:border-l-gray-500'
+                    } border-l-4 duration-300 hover:border-l-4 justify-between items-center p-4 border-b-[1px] bg-slate-100 text-black `}
                   >
                     <div className='flex w-full justify-between items-center'>
                       <div className='flex gap-2'>
-                        <img
-                          className='w-5 h-5 mt-2 opacity-50'
-                          src={item.icon}
-                          alt='Healthcare Icon'
-                        />
+                        {isFeatureSelected(item) ? (
+                          <FaCircleCheck className='text-blue-500 mt-1 text-sm' />
+                        ) : (
+                          <img
+                            className='w-5 h-5 mt-2 opacity-50'
+                            src={item.icon}
+                            alt='Healthcare Icon'
+                          />
+                        )}
                         <div>
                           <p className='text-sm'>{item.name}</p>
                           <p className='text-gray-500 text-xs'>
@@ -311,12 +134,23 @@ export default function App () {
                       <div className='flex gap-x-2'>
                         <div
                           onClick={() => handleFeatureSelection(item)}
-                          className='bg-blue-700 p-2 rounded-md'
+                          className={`${
+                            selectedFeature?.name === item.name
+                              ? 'bg-blue-500'
+                              : 'bg-gray-300'
+                          } p-2 rounded-md duration-300`}
                         >
                           <AiOutlineEye className='text-white' />
                         </div>
-                        <div className='bg-slate-300 p-2 rounded-md'>
-                          <FiPlus className='text-white' />
+                        <div
+                          onClick={() => handleFeaturesSelection(item)}
+                          className='bg-gray-300 p-2 rounded-md'
+                        >
+                          {isFeatureSelected(item) ? (
+                            <MdDeleteOutline className='text-white' />
+                          ) : (
+                            <FiPlus className='text-white' />
+                          )}
                         </div>
                       </div>
                     </div>
@@ -328,13 +162,17 @@ export default function App () {
       </div>
 
       {/* Playground Area */}
-      <div className='flex-1 bg-slate-100'>
+      <div className='flex-1 overflow-y-hidden bg-slate-100 duration-1000'>
         {selectedFeature ? (
-          <div className='flex justify-center h-3/4 items-center gap-x-6'>
+          <div
+            className={`flex justify-center ${
+              selectedFeatures?.length > 0 ? 'h-3/4' : 'h-full'
+            } items-center gap-x-6`}
+          >
             <div className='bg-white p-3 rounded-3xl border-4 border-slate-200 w-48 h-96'>
               <img
                 src={selectedFeature.mobile}
-                alt='mobile'
+                alt='icon'
                 className=' object-fill w-full h-full rounded-md'
               />
             </div>
@@ -342,8 +180,15 @@ export default function App () {
             <div className='w-1/3'>
               <div className='flex items-center gap-2'>
                 <p className='text-black text-lg'>{selectedFeature.name}</p>
-                <div className='bg-white w-7 h-7 items-center justify-center flex border-[1px] cursor-pointer'>
-                  <IoMdAdd className='text-black' />
+                <div
+                  onClick={() => handleFeaturesSelection(selectedFeature)}
+                  className='bg-white w-7 h-7 items-center justify-center flex border-[1px] cursor-pointer'
+                >
+                  {isFeatureSelected(selectedFeature) ? (
+                    <MdDeleteOutline className='text-black' />
+                  ) : (
+                    <FiPlus className='text-black' />
+                  )}
                 </div>
               </div>
               <p className='text-gray-500 py-1 text-xs'>
@@ -361,12 +206,16 @@ export default function App () {
                 {selectedFeature.details}
               </p>
               <div className='bg-white w-24 h-8 items-center justify-center flex border-[1px] cursor-pointer'>
-                <p className='text-black text-xs'>Add to cart</p>
+                <p className='text-black text-xs'>Add note</p>
               </div>
             </div>
           </div>
         ) : (
-          <div className='flex flex-col justify-center items-center h-full'>
+          <div className='flex flex-col relative justify-center items-center h-full'>
+            <div className='absolute left-4'>
+              <FaArrowLeftLong className='text-black text-xl' />
+            </div>
+
             <p className='text-black text-2xl font-bold'>
               No features selected
             </p>
@@ -375,11 +224,19 @@ export default function App () {
             </p>
           </div>
         )}
-        <div className='h-1/4 bg-slate-300'>
+        {/* {selectedFeatures?.length > 0 && ( */}
+        <div
+          className={`h-1/4 ${
+            selectedFeatures?.length > 0 ? 'animate-moveUp' : 'animate-moveDown'
+          } bg-gray-400 duration-300`}
+        >
           <div className='flex justify-center items-center h-full'>
-            <p className='text-white text-2xl'>Playground Area</p>
+            <p className='text-white text-2xl'>
+              Length: {selectedFeatures?.length}
+            </p>
           </div>
         </div>
+        {/* )} */}
       </div>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,30 @@ module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
+      animation: {
+        moveUp: 'moveUp 0.3s ease-in-out',
+        moveDown: 'moveDown 0.3s ease-in-out'
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))'
       },
-    },
+      keyframes: {
+        moveUp: {
+          '0%': { transform: 'translateY(100%)' },
+          '100%': { transform: 'translateY(0)' }
+        },
+        moveDown: {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(100%)' }
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
