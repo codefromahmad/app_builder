9e2b8c414d4e2f9ad949275051f3b46f93be27a8
Aafaq Ahmad <malikafaq788@gmail.com>
2024-02-23 15:34:24 +0500

    mobile and web switch

--- app/components/FeatureHeader.js
+++ app/components/FeatureHeader.js
@@ -6,44 +6,36 @@ const FeatureHeader = ({ platform, setPlatform }) => {
   return (
     <div className="h-16 w-full flex items-center">
       <div className={`px-7 w-full flex justify-between items-center`}>
-        <div className={` flex items-center gap-2`}>
+        <div className={`flex rounded border-gray-200 border items-center`}>
           <div
             onClick={() => setPlatform("mobile")}
-            className={`flex justify-center items-center gap-2 ${
-              platform === "mobile" && "bg-[#00191D]"
-            } px-3 py-2 rounded-md cursor-pointer`}
+            className={`flex justify-center items-center gap-2 rounded-bl rounded-tl ${
+              platform === "mobile" && "bg-slate-200"
+            } px-4 py-3 cursor-pointer`}
           >
-            <FaMobileAlt
-              className={`${
-                platform === "mobile" ? "text-white" : "text-black"
-              }`}
-            />
-            <p
+            <FaMobileAlt className={`text-black`} />
+            {/* <p
               className={`${
                 platform === "mobile" ? "text-white" : "text-black"
               } text-sm`}
             >
               Mobile
-            </p>
+            </p> */}
           </div>
           <div
             onClick={() => setPlatform("desktop")}
-            className={`flex justify-center items-center gap-2 ${
-              platform === "desktop" && "bg-[#00191D]"
-            } px-3 py-2 rounded-md cursor-pointer`}
+            className={`flex justify-center items-center gap-2 rounded-br rounded-tr ${
+              platform === "desktop" && "bg-slate-200"
+            } px-4 py-3 cursor-pointer`}
           >
-            <IoDesktopOutline
-              className={`${
-                platform === "desktop" ? "text-white" : "text-black"
-              }`}
-            />
-            <p
+            <IoDesktopOutline className={`text-black`} />
+            {/* <p
               className={`${
                 platform === "desktop" ? "text-white" : "text-black"
               } text-sm`}
             >
               Web
-            </p>
+            </p> */}
           </div>
         </div>
         {/* <div
